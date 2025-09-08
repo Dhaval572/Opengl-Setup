1ba28e04e91f205bd8373a7d2bfc4bf0b49272d4
Dhaval <dp9718776@gmail.com>
2025-09-08 10:14:57 +0530

    Update Opengl version

--- src/main.cpp
+++ src/main.cpp
@@ -39,8 +39,8 @@ void main()
 int main()
 {
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLFWwindow* window = glfwCreateWindow
